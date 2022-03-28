5181e08020887ffdbb326e52364f9f5a676d1984
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-03-27 21:24:17 -0400

    implement App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MessageWindow from './MessageWindow.jsx';
+import TextBar from './TextBar.jsx';
+import { registerOnMessageCallback, send, startWebsocketConnection } from './websocket';
+
+/*
+* TODO:
+* Oauth login
+* allow for persistence of messages
+* implement styledcomponents
+*/
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    startWebsocketConnection();
+  }, []);
+
+  const onMessageReceived = (msg) => {
+    msg = JSON.parse(msg);
+    setMessages([...messages, msg]);
+    console.log('messages received: ', messages)
+  }
+
+  registerOnMessageCallback(onMessageReceived.bind(this));
+
+  const sendMessage = (text) => {
+    const message = {
+      username,
+      text,
+    };
+    send(JSON.stringify(message));
+  };
+
+  if ( username === null ) {
+    return(
+      <div className='container'>
+        <div className='container-title'>Enter username</div>
+        <TextBar onSend={setUsername}/>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='container-title'>Messages</div>
+      <MessageWindow messages={messages} username={username} />
+      <TextBar onSend={sendMessage}/>
     </div>
   );
 }
